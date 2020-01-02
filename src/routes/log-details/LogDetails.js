2e7fea89ac0931972e8e195183b611df7632102a
friedcode <fried.m1994@gmail.com>
2020-01-02 12:30:00 +0100

    changed grid layout

--- src/routes/log-details/LogDetails.js
+++ src/routes/log-details/LogDetails.js
@@ -22,19 +22,32 @@ class LogDetails extends React.Component {
 
   render() {
     return (
-      <div className={"details-list"}>
-        <h4>Navigation</h4>
-        {this.props.selectedLog.content.navigation
-          ? Object.keys(this.props.selectedLog.content.navigation).map(key => {
-            return (<LogDetail key={key} name={key} detail={this.props.selectedLog.content.navigation[key]}/>)
-          })
-          : 'Navigation Details not found!'}
-        <h4>Environment</h4>
-        {this.props.selectedLog.content.environment
-          ? Object.keys(this.props.selectedLog.content.environment).map(key => {
-            return (<LogDetail key={key} name={key} detail={this.props.selectedLog.content.environment[key]}/>)
-          })
-          : 'Environment Details not found!'}
+      <div className={"desktop-columns"}>
+        <div className={"details-list"}>
+          <h4>Navigation</h4>
+          {this.props.selectedLog.content.navigation
+            ? Object.keys(this.props.selectedLog.content.navigation).map(key => {
+              return (<LogDetail key={key} name={key} detail={this.props.selectedLog.content.navigation[key]}/>)
+            })
+            : 'Navigation Details not found!'}
+        </div>
+        <div className={"details-list"}>
+          <h4>Environment</h4>
+          {this.props.selectedLog.content.environment
+            ? Object.keys(this.props.selectedLog.content.environment).map(key => {
+              return (<LogDetail key={key} name={key} detail={this.props.selectedLog.content.environment[key]}/>)
+            })
+            : 'Environment Details not found!'}
+        </div>
+        <div className={"details-list"}>
+          <h4>Electrical</h4>
+          {this.props.selectedLog.content.electrical
+            ? Object.keys(this.props.selectedLog.content.electrical).map(key => {
+              return (<LogDetail key={key} name={key} detail={this.props.selectedLog.content.electrical[key]}/>)
+            })
+            : 'Environment Details not found!'}
+        </div>
+
       </div>
     )
 
