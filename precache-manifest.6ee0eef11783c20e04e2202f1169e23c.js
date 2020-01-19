fd9d16151d01f175ac8b32000d55878870f6798f
friedcode <fried.m1994@gmail.com>
2020-01-19 13:46:44 +0100

    Updates

---
+++ precache-manifest.6ee0eef11783c20e04e2202f1169e23c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ba32a08e68232f62f1b44a6d423262b",
+    "url": "/progressive-sailing-react/index.html"
+  },
+  {
+    "revision": "fd882504c647734d3c60",
+    "url": "/progressive-sailing-react/static/css/0.d32ddac6.chunk.css"
+  },
+  {
+    "revision": "7e9de50dd29da310ee85",
+    "url": "/progressive-sailing-react/static/css/4.f8e6e8e4.chunk.css"
+  },
+  {
+    "revision": "4aa86d878dc705aa7efd",
+    "url": "/progressive-sailing-react/static/css/main.9b62bdc3.chunk.css"
+  },
+  {
+    "revision": "fd882504c647734d3c60",
+    "url": "/progressive-sailing-react/static/js/0.61839f52.chunk.js"
+  },
+  {
+    "revision": "810828d45190606626c5",
+    "url": "/progressive-sailing-react/static/js/3.71868b92.chunk.js"
+  },
+  {
+    "revision": "f5de2b06336b008edfdba5dff51cdc0c",
+    "url": "/progressive-sailing-react/static/js/3.71868b92.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7e9de50dd29da310ee85",
+    "url": "/progressive-sailing-react/static/js/4.af28b9e6.chunk.js"
+  },
+  {
+    "revision": "4aa86d878dc705aa7efd",
+    "url": "/progressive-sailing-react/static/js/main.c909e546.chunk.js"
+  },
+  {
+    "revision": "533bc372aa1b1410ee84",
+    "url": "/progressive-sailing-react/static/js/runtime-main.730f3b7e.js"
+  }
+]);
